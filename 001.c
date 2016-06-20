545daf6ae008c2c310d5325b61c0013b0ee7be5d
marcialwushu <marcialwushu@gmail.com>
2016-06-20 18:47:29 -0300

    logica de programação

---
+++ 001.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	int vet[5],i,count=0;
+
+	for(i=0;i<4;i++){
+		printf("Digite um valor para o vetor \n");
+		scanf("%d",&vet[i]);
+	}
+	for(i=1;i<5;i++){
+		if(vet[i]>i){
+			count=count+1;
+		}
+	}
+	
+	printf("posição eh %d : ",count);
+
+	return 0;
+}
+	
