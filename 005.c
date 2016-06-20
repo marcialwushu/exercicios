28381b6d7ac59f830b74e7712e781278a4cdb831
marcialwushu <marcialwushu@gmail.com>
2016-06-20 18:55:46 -0300

    logica de programação

---
+++ 005.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+	int vet[5],i,j,aux;
+
+	for(i=0;i<4;i++){
+		printf("Digite um valor \n");
+		scanf("%d",&vet[i]);
+	}
+
+	for(i=0;i<4;i++){
+		for(j=i+1;j<5;++j){
+			if(vet[i]>vet[j]){
+				aux=vet[i];
+				vet[i]=vet[j];
+				vet[j]=aux;
+
+			}		
+		}
+	}
+
+	printf("A ordem inversa do vetor eh \n");
+
+	for(i=0;i<4;++i){
+		printf("%d",vet[i]);
+	}
+
+	return 0;
+
+}
+
+
